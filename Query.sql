d77845c4880c8b1aec32956e84a9bd9d9ae5a1ce
aklima773 <aklimaa773@gmail.com>
2026-08-30 22:52:09 +0600

    create matches table

--- Query.sql
+++ Query.sql
@@ -8,3 +8,17 @@ CREATE TABLE users (
         CHECK (role IN ('Ticket Manager', 'Football Fan')),
     phone_number VARCHAR(20)
 );
+
+---2. CREATE MATCHES TABLE
+
+CREATE TABLE matches (
+    match_id INT PRIMARY KEY,
+    fixture VARCHAR(150) NOT NULL,
+    tournament_category VARCHAR(100) NOT NULL,
+    base_ticket_price NUMERIC(10, 2) NOT NULL
+        CONSTRAINT positive_ticket_price CHECK (base_ticket_price >= 0),
+    match_status VARCHAR(30) NOT NULL
+        CONSTRAINT valid_match_status CHECK (
+            match_status IN ('Available', 'Selling Fast', 'Sold Out', 'Postponed')
+        )
+);
