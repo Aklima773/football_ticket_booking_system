1b7ff46268510b373f390313141c83b73983c847
aklima773 <aklimaa773@gmail.com>
2026-08-30 23:54:57 +0600

    search all users whose full names start with Tanvir or Haque

--- Query.sql
+++ Query.sql
@@ -66,3 +66,18 @@ VALUES
     (503, 2, 101, 'A-13', 'Confirmed', 150),
     (504, 2, 101, NULL, NULL, 150),
     (505, 3, 102, 'C-20', 'Pending', 120); 
+
+    
+---Q:1  Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+
+SELECT match_id, fixture, base_ticket_price
+FROM matches
+WHERE tournament_category ='Champions League'
+AND match_status = 'Available';
+
+---Q:2 Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive)
+
+SELECT user_id,full_name,email
+FROM users
+WHERE full_name LIKE 'Tanvir%'
+OR full_name ILIKE '%Haque%';
